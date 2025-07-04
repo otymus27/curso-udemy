e95b3c943fcf24e780d13d9e3be2928766effeb4
Fábio <otymus27@gmail.com>
2025-07-04 12:15:15 -0300

    Primeiro commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { helloWorld } from './components/primeiro-componente/primeiro-componente';
+import { Calculadora } from "./components/calculadora/calculadora";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [helloWorld, Calculadora],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected title = 'conceitos-basicos';
---
+++ src/app/components/primeiro-componente/primeiro-componente.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  // O selector serve para referenciar este componente ou dar nome pra ele
+  selector: 'app-hello-world',
+  imports: [],
+  templateUrl: './primeiro-componente.html',
+  styleUrl: './primeiro-componente.scss',
+})
+export class helloWorld {}
